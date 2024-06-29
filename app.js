76a01f9c7919e997dbb0a9fb6a0e1ae734f95535
TrieuVD <trieuvd@runsystem.net>
2024-06-29 21:04:48 +0700

    change routes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
+app.get('/hello', function (req, res) {
   res.send('Hello World!');
 });
 app.listen(3000, function () {
